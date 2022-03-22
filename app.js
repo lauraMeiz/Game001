6bda4249fd56e2cf1233331a5dabef2f6ca388e5
Laura <97021990+lauraMeiz@users.noreply.github.com>
2022-03-22 14:40:30 +0200

    reset commit

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ function rand(min, max) {
 }
 
 const timer = document.querySelector(".time");
-console.log(timer);
 const rutuliukuVieta = document.querySelector(".left-square");
-console.log(rutuliukuVieta);
 const btn1 = document.querySelector(".btn1");
-console.log(btn1);
 
 btn1.addEventListener("click", () => {
   const rutuliukai = [];
+
   while (rutuliukai.length < 25) {
     const rutulys = rand(1, 25);
     if (!rutuliukai.includes(rutulys)) {
       rutuliukai.push(rutulys);
+    } else {
+      btn1.setAttribute("disabled", "");
     }
   }
 
@@ -48,37 +48,24 @@ btn1.addEventListener("click", () => {
       const s = clockTime % 60;
       timer.innerText = ` ${m < 10 ? "0" + m : m} : ${s < 10 ? "0" + s : s}`;
     }
+    const my = setInterval(updateClock, 1000);
 
-    setInterval(updateClock, 1000);
     const btn2 = document.querySelector(".btn2");
+
     btn2.addEventListener("click", () => {
-      if (clockTime > 0) {
-        clockTime = 0;
-      }
-      clockTime--;
-      const m = 0;
-      const s = 0;
-      timer.innerText = ` ${m ? 0 : "00"} : ${s ? 0 : "00"}`;
-      // if (rutuliukai.length === 25) {
-
-      // }
-
-      // pabaiga;
+      removeBalls();
+      myStopFunction();
     });
 
-    // function stop() {
-    //   const stopLaikas = document.querySelector(".time");
-    //   const tekstas = document.createTextNode("00 : 00");
-    //   stopLaikas.appendChild(tekstas);
-    //   clockTime--;
-    // }
-    // pabaiga = setTimeout(stop, 100000);
+    //console.log(rutuliukai);
+
+    function removeBalls() {
+      rutuliukuVieta.style.display = "none";
+    }
+
+    function myStopFunction() {
+      clearInterval(my);
+      timer.innerText = `00 : 00`;
+    }
   });
-
-  // const btn2 = document.querySelector(".btn2");
-  // btn2.addEventListener("click", () => {
-
-  //   clockTime--;
-  // });
-  console.log(rutuliukai);
 });
